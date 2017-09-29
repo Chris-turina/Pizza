63f18fa20b152b0dfcd564b0632a39fbc55b351c
chris turina <chris.turina@gmail.com>
2017-09-29 08:38:02 -0700

    create pizza object

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+// Pizza Constructor and Methods
+function Pizza() {
+  this.size = size;
+  this.toppings = [];
+  this.sides = sides;
+}
+
+Pizza.prototype.Cost = function () {
+  var toppingsAdded = this
+
+};
+
+};
+
+// User Interface
+$(document).ready(function() {
+  $('select').material_select();
+  $("form#pizza")
+});
