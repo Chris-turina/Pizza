01ba0a4c124b7a15481922e75f12b3426f989cec
chris turina <chris.turina@gmail.com>
2017-09-29 09:09:52 -0700

    create buttons, switches, for test of cost

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,13 @@
 // Pizza Constructor and Methods
-function Pizza() {
+function Pizza(size) {
   this.size = size;
   this.toppings = [];
-  this.sides = sides;
 }
 
 Pizza.prototype.Cost = function () {
-  var toppingsAdded = this
+  var toppingsAdded = this.toppings.length * 2;
+  var total = this.size + toppingsAdded;
+  return total;
 
 };
 
@@ -15,5 +16,13 @@ Pizza.prototype.Cost = function () {
 // User Interface
 $(document).ready(function() {
   $('select').material_select();
-  $("form#pizza")
+  $("form#pizza").submit(function(event) {
+    var pizzaSize = parseInt($("input[type='radio'][name='pizza-size']:checked").val());
+    var newPizza = new Pizza(sizeOption);
+    $.each($("input[name='toppings']:checked"), function() {
+      newPizza.toppings.push($(this).val());
+    })
+  var orderTotal = newPizza.Cost();
+  $("#order-total").text(orderTotal);
+  });
 });
